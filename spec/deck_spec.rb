45685d34bee3da89d80812d9e176b1f4cfad61e4
chadc76 <chadcunningham7676@gmail.com>
2020-11-10 21:31:57 +0000

    Completed spec for Deck#initialize method

---
+++ spec/deck_spec.rb
@@ -0,0 +1,19 @@
+require 'deck'
+
+describe Deck do
+  subject(:deck) {Deck.new}
+
+  describe "#initialize" do
+    it "sets up cards in an array" do 
+      expect(deck.cards).to be_an_instance_of(Array)
+    end
+
+    it "sets each card to be a card object" do
+      double(:card) {Card.new}
+
+      deck.cards.each do |card|
+        expect(card).to be_an_instance_of(Card)
+      end
+    end
+  end
+end
