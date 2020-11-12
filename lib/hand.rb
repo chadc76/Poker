3e45e459490904f7217f0c46eacde01d702061ee
chadc76 <chadcunningham7676@gmail.com>
2020-11-12 17:07:12 +0000

    Completed Hand#take method

--- lib/hand.rb
+++ lib/hand.rb
@@ -6,5 +6,12 @@ class Hand
     @cards = cards
   end
 
+  def take(throw_away, new_cards)
+    raise 'must have five cards' unless throw_away.length == new_cards.length
+    raise 'cannot discard unowned card' unless throw_away.all?{|card| @cards.include?(card)}
+    @cards.reject!{|card| throw_away.include?(card)}
+    @cards += new_cards
+  end
+
 end
 
