983546d99586d2496eda94ca0be92302681c75f5
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-11 22:32:22 -0500

    added error raising to initialize spec and refactored

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,23 @@
 require 'card'
 
 describe Card do
-  subject(:ace) {Card.new("Ace", "Spade")}
+  subject(:ace) {Card.new(:ace, :spades)}
 
   describe "#initialize" do
-    it "sets the card type" do
-      expect(ace.type).to eq("Ace")
+    it "sets the card value" do
+      expect(ace.value).to eq(:ace)
     end
 
     it "sets the card suit" do
-      expect(ace.suit).to eq("Spade")
+      expect(ace.suit).to eq(:spades)
+    end
+
+    it "raise an error when recieving invalid suit" do
+      expect { Card.new(:ace, :wrong) }.to raise_error("illegal suit or value")
+    end
+
+    it "raise an error when recieving invalid value" do
+      expect { Card.new(:wrong, :spade) }.to raise_error("illegal suit or value")
     end
   end
 end
