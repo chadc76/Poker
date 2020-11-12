b478fa690c90e37044b91e6c8e5797a8a73861f5
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-11 22:45:52 -0500

    Added Deck::all_cards spec and refactored other specs

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,23 +1,42 @@
 require 'deck'
 
 describe Deck do
-  subject(:deck) {Deck.new}
-
-  describe "#initialize" do
-    it "sets up cards in an array" do 
-      expect(deck.cards).to be_an_instance_of(Array)
+  describe "::all_cards" do 
+    subject(:all_cards) {Deck.all_cards}
+    
+    it "should generate 52 cards" do
+      expect(all_cards.count).to eq(52)
     end
 
-    it "sets each card to be a card object" do
-      double(:card) {Card.new}
-
-      deck.cards.each do |card|
-        expect(card).to be_an_instance_of(Card)
-      end
+    it "returns all cards without duplicates" do 
+      expect(
+        all_cards.map { |card| [card.suit, card.value] }.uniq.count
+      ).to eq(all_cards.count)
     end
   end
 
+  let(:cards) do
+    [ double("card", :suit => :spades, :value => :king),
+      double("card", :suit => :spades, :value => :queen),
+      double("card", :suit => :spades, :value => :jack) ]
+  end
+
+  describe "#initialize" do
+    it "by defualt fills cards with 52 cards" do
+      deck = Deck.new 
+      expect(deck.cards.count).to eq(52)
+    end
+
+    it "should be able to take in an array of cards" do
+      deck = Deck.new(cards)
+      expect(deck.cards.count).to eq(3)
+    end
+  end
+
+  subject(:deck) { Deck.new }
+
   describe "#shuffle" do
+    
     it "shuffles the deck of cards" do
       unshuffled = deck.cards.dup
       deck.shuffle 
