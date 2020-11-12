e828a3fc5e5b83099780aa805d799b7d7b6a2c3e
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-11 22:33:28 -0500

    Added new Class Methods and Class Constants as well as added a error raise in initialize method

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,47 @@
 class Card
-  attr_reader :type, :suit
-  def initialize(type, suit)
-    @type = type
+
+  SUIT_STRINGS = {
+    :clubs    => "♣",
+    :diamonds => "♦",
+    :hearts   => "♥",
+    :spades   => "♠"
+  }
+
+  VALUES_STRINGS = {
+    :two   => "2",
+    :three => "3",
+    :four  => "4",
+    :five  => "5",
+    :six   => "6",
+    :seven => "7",
+    :eight => "8",
+    :nine  => "9",
+    :ten   => "10",
+    :jack  => "J",
+    :queen => "Q",
+    :king  => "K",
+    :ace   => "A"
+  }
+
+  def self.suits
+    SUIT_STRINGS.keys
+  end
+
+  def self.values
+    VALUES_STRINGS.keys
+  end
+
+  def self.Royal_values
+    VALUES_STRINGS.keys[-5..-1]
+  end
+
+  attr_reader :value, :suit
+
+  def initialize(value, suit)
+    unless Card.suits.include?(suit) && Card.values.include?(value)
+      raise "illegal suit or value"
+    end
+    @value = value
     @suit = suit
   end
 end
