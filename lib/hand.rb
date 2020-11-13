4cb0eec676a7ad74a02f3453912ad00ae7d51702
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-13 17:50:58 -0500

    Refactored method Hand#take to be Hand#trade_away and added helper methods

--- lib/hand.rb
+++ lib/hand.rb
@@ -6,12 +6,29 @@ class Hand
     @cards = cards
   end
 
-  def take(throw_away, new_cards)
-    raise 'must have five cards' unless throw_away.length == new_cards.length
-    raise 'cannot discard unowned card' unless throw_away.all?{|card| @cards.include?(card)}
-    @cards.reject!{|card| throw_away.include?(card)}
+  def trade_cards(old_cards, new_cards)
+    raise 'must have five cards' unless old_cards.length == new_cards.length
+    raise 'cannot discard unowned card' unless has_cards?(old_cards)
+    take_cards(new_cards) && discard_cards(old_cards) && sort!
+    old_cards
+  end
+
+  private
+
+  def sort!
+    @cards.sort!
+  end
+
+  def take_cards(new_cards)
     @cards += new_cards
   end
 
+  def discard_cards(old_cards)
+    @cards.reject!{|card| old_cards.include?(card)}
+  end
+
+  def has_cards?(old_cards)
+    old_cards.all?{|card| @cards.include?(card)}
+  end
 end
 
