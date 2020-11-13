2e26a4c15e83e0d6e14d4fed07ae991b588bc7fa
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-13 17:50:30 -0500

    Refactored method Hand#take to be Hand#trade_away and added tests

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -22,25 +22,31 @@ describe Hand do
     end
   end
 
-  describe "#take" do 
-    let(:throw_away) { cards[0..1] }
+  describe "#trade_cards" do 
+    let(:old_cards) { cards[0..1] }
     let(:new_cards) { [Card.new(:five, :spades), Card.new(:three, :clubs)] }
 
     it "discards specified cards" do 
-      expect(hand.take(throw_away, new_cards)).to_not include(*throw_away)
+      hand.trade_cards(old_cards, new_cards)
+      expect(hand.cards).to_not include(*old_cards)
     end
 
-    it "adds new cards to hand" do 
-      expect(hand.take(throw_away, new_cards)).to include(*new_cards)
+    it "adds new cards to hand" do
+      hand.trade_cards(old_cards, new_cards) 
+      expect(hand.cards).to include(*new_cards)
     end
 
     it 'raises an error if trade does not result in five cards' do
-      expect { hand.take(throw_away, new_cards[0..0]) }.to raise_error('must have five cards')
-      expect { hand.take(throw_away[0..0], new_cards) }.to raise_error('must have five cards')
+      expect { hand.trade_cards(old_cards, new_cards[0..0]) }.to raise_error('must have five cards')
+      expect { hand.trade_cards(old_cards[0..0], new_cards) }.to raise_error('must have five cards')
     end
 
     it 'raises error if it tries to trade a card that is not owned' do
-      expect { hand.take([Card.new(:ten, :hearts)], new_cards[0..0]) }.to raise_error('cannot discard unowned card')
+      expect { hand.trade_cards([Card.new(:ten, :hearts)], new_cards[0..0]) }.to raise_error('cannot discard unowned card')
+    end
+
+    it 'returns old cards' do 
+      expect(hand.trade_cards(old_cards, new_cards)).to eq(old_cards)
     end
   end
 end
