609c18933b445de7ef428e8616a7768258fe5381
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-13 20:37:24 -0500

    Completed Hand#<=> spec

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -327,6 +327,200 @@ describe Hand do
             expect(ace_with_two <=> ace_four).to eq(-1)
           end
 
+          let(:ace_with_two_hearts) do
+            Hand.new([
+              Card.new(:ace, :spades),
+              Card.new(:ace, :hearts),
+              Card.new(:ace, :diamonds),
+              Card.new(:ace, :clubs),
+              Card.new(:two, :hearts)
+            ])
+          end
+
+          it 'returns 0 when high cards are the same but a different suit' do 
+            expect(ace_with_two <=> ace_with_two_hearts).to eq(0)
+          end
+        end
+
+        context 'two pairs' do
+          let(:two_pair_3_4) do
+            Hand.new([
+              Card.new(:three, :spades),
+              Card.new(:three, :hearts),
+              Card.new(:four, :hearts),
+              Card.new(:four, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          let(:two_pair_3_4_diamonds) do
+            Hand.new([
+              Card.new(:three, :spades),
+              Card.new(:three, :hearts),
+              Card.new(:four, :diamonds),
+              Card.new(:four, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          let(:two_pair_3_4_9) do
+            Hand.new([
+              Card.new(:three, :spades),
+              Card.new(:three, :hearts),
+              Card.new(:four, :hearts),
+              Card.new(:four, :diamonds),
+              Card.new(:nine, :hearts)
+            ])
+          end
+
+          let(:two_pair_4_5) do
+            Hand.new([
+              Card.new(:five, :spades),
+              Card.new(:five, :hearts),
+              Card.new(:four, :hearts),
+              Card.new(:four, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+          let(:two_pair_2_5) do
+            Hand.new([
+              Card.new(:five, :spades),
+              Card.new(:five, :hearts),
+              Card.new(:two, :hearts),
+              Card.new(:two, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          let(:pair_of_sixes) do
+            Hand.new([
+              Card.new(:six, :spades),
+              Card.new(:six, :hearts),
+              Card.new(:four, :hearts),
+              Card.new(:five, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          it 'should have two pair beats one pair' do 
+            expect(two_pair_4_5 <=> pair_of_sixes).to eq(1)
+          end
+
+          it 'should have higher of two pairs to win' do
+            expect(two_pair_4_5 <=> two_pair_3_4).to eq(1)
+          end
+
+          it 'should have higher second pair win, if high pair is the same' do 
+            expect(two_pair_4_5 <=> two_pair_2_5).to eq(1)
+          end
+
+          it 'should have higher 5th card decide winner if same pairs' do
+            expect(two_pair_3_4 <=> two_pair_3_4_9).to eq(1)
+            expect(two_pair_3_4_9 <=> two_pair_3_4).to eq(-1)
+          end
+          it 'should return 0 if same card values, but different suits' do
+            expect(two_pair_3_4 <=> two_pair_3_4_diamonds).to eq(0)
+          end
+        end
+
+        context 'one pair' do 
+          let(:ace_pair) do
+            Hand.new([
+              Card.new(:ace, :spades),
+              Card.new(:ace, :spades),
+              Card.new(:queen, :hearts),
+              Card.new(:jack, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          let(:king_pair) do
+            Hand.new([
+              Card.new(:king, :spades),
+              Card.new(:king, :spades),
+              Card.new(:queen, :hearts),
+              Card.new(:jack, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          let(:three_pair_jack_high) do
+            Hand.new([
+              Card.new(:three, :spades),
+              Card.new(:three, :hearts),
+              Card.new(:nine, :diamonds),
+              Card.new(:jack, :hearts),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          let(:three_pair_king_high) do
+            Hand.new([
+              Card.new(:three, :spades),
+              Card.new(:three, :hearts),
+              Card.new(:nine, :diamonds),
+              Card.new(:king, :hearts),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          let(:four_pair) do
+            Hand.new([
+              Card.new(:four, :spades),
+              Card.new(:four, :hearts),
+              Card.new(:ace, :diamonds),
+              Card.new(:two, :hearts),
+              Card.new(:three, :hearts)
+            ])
+          end
+
+          it 'should compare based on pair value' do 
+            expect(ace_pair <=> king_pair).to eq(1)
+            expect(four_pair <=> three_pair_jack_high).to eq(1)
+          end
+
+          let(:ace_pair_king_high) do
+            Hand.new([
+              Card.new(:ace, :spades),
+              Card.new(:ace, :spades),
+              Card.new(:king, :hearts),
+              Card.new(:jack, :diamonds),
+              Card.new(:ten, :hearts)
+            ])
+          end
+
+          it 'should compare based on high card if same pair value' do
+            expect(ace_pair_king_high <=> ace_pair).to eq(1)
+            expect(three_pair_king_high <=> three_pair_jack_high).to eq(1)
+          end
+        end
+
+        context 'high card' do
+          let(:ten_high) do
+            Hand.new([
+              Card.new(:two, :spades),
+              Card.new(:four, :hearts),
+              Card.new(:six, :diamonds),
+              Card.new(:nine, :spades),
+              Card.new(:ten, :spades)
+            ])
+          end
+
+          let(:king_high) do
+            Hand.new([
+              Card.new(:two, :spades),
+              Card.new(:four, :hearts),
+              Card.new(:six, :diamonds),
+              Card.new(:nine, :spades),
+              Card.new(:king, :spades)
+            ])
+          end
+
+          it 'should compare based on high card' do
+            expect(king_high <=> ten_high).to eq(1)
+            expect(ten_high <=> king_high).to eq(-1)
+            expect(king_high <=> king_high).to eq(0)
+          end
         end
       end
     end
