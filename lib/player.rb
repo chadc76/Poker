03e03e6fd44db464185760eea80a1acff0ccbf74
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-13 21:26:19 -0500

    Completed Player::buy_in, Player#deal_in, and Player#take_bet methods

---
+++ lib/player.rb
@@ -0,0 +1,26 @@
+class Player
+  include Comparable
+
+  def self.buy_in(bankroll)
+    Player.new(bankroll)
+  end
+
+  attr_reader :bankroll, :hand, :current_bet
+
+  def initialize(bankroll)
+    @bankroll = bankroll
+    @current_bet = 0
+  end
+
+  def deal_in(hand)
+    @hand = hand
+  end
+
+  def take_bet(bet)
+    amount = bet - current_bet
+    raise 'not enough money' unless amount <= bankroll
+    @bankroll -= amount
+    @current_bet = amount
+    amount
+  end
+end
