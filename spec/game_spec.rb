ee5d3c8935df3c536945a9adb57981f536829814
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-13 22:11:43 -0500

    Completed Game#add_players method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,4 +18,26 @@ describe Game do
       expect(game.deck.count).to eq(52)
     end
   end
+
+  describe '#add_players' do
+    it 'should create the specified amount of players' do 
+      game.add_players(2, 100)
+      expect(game.players.length).to eq(2)
+    end
+
+    it 'should created players' do 
+      game.add_players(3, 100)
+      expect(
+          game.players.all?{|player| player.is_a?(Player)}
+      ).to be(true)
+    end
+
+    it 'should set players bankrolls' do
+      game.add_players(3, 100)
+      expect(
+          game.players.all? { |player| player.bankroll == 100 }
+      ).to be(true)
+    end
+
+  end
 end
