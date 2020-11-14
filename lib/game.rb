f79a935aed47c847a02d5ddc1529666ab0ffd953
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-13 22:11:55 -0500

    Completed Game#add_players specs

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,17 @@ require_relative 'player.rb'
 require_relative 'deck.rb'
 
 class Game
-  attr_reader :pot, :deck
+  attr_reader :pot, :deck, :players
 
   def initialize
     @pot = 0
     @deck = Deck.new
+    @players = []
+  end
+
+  def add_players(n, bankroll)
+    n.times do 
+      @players << Player.new(bankroll)
+    end
   end
 end
