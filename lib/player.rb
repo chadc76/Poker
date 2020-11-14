b0b8f9d9ed5c9132cb6d2ba72790ceac761efd98
Chad Cunningham <chadcunningham7676@gmail.com>
2020-11-13 21:52:56 -0500

    Completed Player class

--- lib/player.rb
+++ lib/player.rb
@@ -16,6 +16,34 @@ class Player
     @hand = hand
   end
 
+  def respond_bet
+    print "(c)all, (b)et, or (f)old? > "
+    response = gets.chomp.downcase[0]
+    case response
+    when 'c' then :call
+    when 'b' then :bet
+    when 'f' then :fold
+    else
+      puts 'must be (c)all, (b)et, or (f)old'
+      respond_bet
+    end
+  end
+
+  def get_bet
+    print "Bet (bankroll: $#{bankroll}) > "
+    bet = gets.chomp.to_i
+    raise 'not enough money' unless bet <= bankroll
+    bet
+  end
+
+  def get_cards_to_trade
+    print "Cards to trade? (ex. '1, 4, 5') > "
+    card_indices = gets.chomp.split(', ').map(&:to_i)
+    raise 'cannot trade more than three cards' unless card_indices.count <= 3
+    puts
+    card_indices.map { |i| hand.cards[i - 1] }
+  end
+
   def take_bet(bet)
     amount = bet - current_bet
     raise 'not enough money' unless amount <= bankroll
@@ -44,6 +72,18 @@ class Player
 
   def folded?
     @folded
+  end
+
+  def trade_cards(old_cards, new_cards)
+    hand.trade_cards(old_cards, new_cards)
+  end
+
+  def <=>(other_player)
+    hand <=> other_player.hand
+  end
+
+  def reset_current_bet
+    @current_bet = 0
   end
 
 end
